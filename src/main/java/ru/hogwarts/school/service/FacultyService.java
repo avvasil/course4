1c8f7cecc8448afc294bf3363438b3c4c35149d4
avvasil <avvasil@mail.ru>
2022-05-30 13:00:22 +0300

    homework4.5

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -3,11 +3,13 @@ package ru.hogwarts.school.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.hogwarts.school.exceptions.RepositoryIsEmptyException;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repositories.FacultyRepository;
 
 import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -62,7 +64,8 @@ public class FacultyService {
     public String getLongestFacultyName() {
         return facultyRepository.findAll().stream()
                 .map(Faculty::getName)
-                .max(Comparator.comparingInt(String::length)).get();
+                .max(Comparator.comparingInt(String::length))
+                .orElseThrow(() -> new RepositoryIsEmptyException("It seems that the repository is empty"));
     }
 
     public int getOptimizedResult() { //Модификация не требуется, поскольку в данном случае, накладные расходы
